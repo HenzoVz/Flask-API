912e168a6bf5d64e36fb535611df7fbdb253344c
murilo henzo <murilohezo@gmail.com>
2019-06-08 12:40:43 -0300

    üpdate

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from model import Classifiers
 
@@ -15,4 +16,5 @@ def predict_iris():
     return str(prediction)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
