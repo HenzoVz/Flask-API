93b52f1a10ac5a14fedb47ce2c9b5049e0105fa0
murilo henzo <murilohezo@gmail.com>
2019-06-08 00:17:56 -0300

    üpdate

--- src/request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# URL
-url = 'http://localhost:8000/predict'
-
-# Change the value of experience that you want to test
-r = requests.post(url,json={'s_length':1.8,'s_width':1.0, 'p_length':2.0, 'p_width':1.0,})
-print(r.json())
