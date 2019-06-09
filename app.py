a0c744a8853ca9363542355c28a8eef0b1d447de
murilo henzo <murilohezo@gmail.com>
2019-06-08 21:56:24 -0300

    update release

--- app.py
+++ app.py
@@ -7,13 +7,24 @@ app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict_iris():
 
-    data1 = request.get_json(force=True)
-    data2 = request.get_json(force=True)
-    data3 = request.get_json(force=True)
-    data4 = request.get_json(force=True)
+    s_length = request.get_json(force=True)
+    s_width = request.get_json(force=True)
+    p_length = request.get_json(force=True)
+    p_width = request.get_json(force=True)
 
-    prediction = Classifiers.Random_Forest([[data1['s_length'], data2['s_width'], data3['p_length'], data4['p_width']]])
-    return str(prediction)
+    prediction = Classifiers.Random_Forest([[s_length['s_length'], s_width['s_width'], p_length['p_length'], p_width['p_width']]])
+    if prediction == [0]:
+       iris_setosa = 'Iris Setosa'
+       print(iris_setosa)
+       return iris_setosa
+    if prediction == [1]:
+       iris_virginica = 'Iris Virginica'
+       print(iris_virginica)
+       return iris_virginica
+    else:
+       iris_versicolor = 'Iris Versicolor'
+       print(iris_versicolor)
+       return iris_versicolor
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
