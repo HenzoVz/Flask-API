db583ab929177e01811d6cc02d009803f8e255dc
murilo henzo <murilohezo@gmail.com>
2019-06-22 19:46:57 -0300

    update code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from model import Classifiers
 
 app = Flask(__name__)
@@ -12,18 +12,15 @@ def predict_iris():
     p_length = request.get_json(force=True)
     p_width = request.get_json(force=True)
 
-    prediction = Classifiers.Random_Forest([[s_length['s_length'], s_width['s_width'], p_length['p_length'], p_width['p_width']]])
+    prediction, accuracy = Classifiers.Random_Forest([[s_length['s_length'], s_width['s_width'], p_length['p_length'], p_width['p_width']]])
     if prediction == [0]:
        iris_setosa = 'Iris Setosa'
-       print(iris_setosa)
        return iris_setosa
     if prediction == [1]:
        iris_virginica = 'Iris Virginica'
-       print(iris_virginica)
        return iris_virginica
     else:
        iris_versicolor = 'Iris Versicolor'
-       print(iris_versicolor)
        return iris_versicolor
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 class Classifiers():
 
@@ -12,6 +13,9 @@ class Classifiers():
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
         clf_forest = RandomForestClassifier(n_estimators=15, max_depth=5)
         clf_forest.fit(X_train, y_train)
-        return clf_forest.predict(data)
+        y_pred = clf_forest.predict(X_test)
+        accuracy = (accuracy_score(y_test, y_pred)*100)
+        return clf_forest.predict(data), accuracy
 
-#print(Classifiers.Random_Forest([[1.8,1,2,1]]))
+
+
