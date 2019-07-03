8dd81434dee3b7f3ee3253635c39eb39234aa771
murilo henzo <murilohezo@gmail.com>
2019-07-02 23:13:05 -0300

    Update test request

--- src/server.py
+++ src/server.py
@@ -12,21 +12,21 @@ def predict_iris():
     data4 = request.get_json(force=True)
 
     prediction = Classifiers.Random_Forest([[data1['s_length'], data2['s_width'], data3['p_length'], data4['p_width']]])
-    print(prediction)
+    #print(prediction)
     #print(type(prediction))
 
     if prediction == [0]:
-       prediction_setosa = '[íris setosa]'
-       print(prediction_setosa)
-       return prediction_setosa
+       prediction_setosa = 'íris setosa'
+       #print(prediction_setosa)
+       return json.dumps(prediction_setosa)
     if prediction == [1]:
-       prediction_virginica = '[íris virginica]'
-       print(prediction_virginica)
-       return prediction_virginica
+       prediction_virginica = 'íris virginica'
+       #print(prediction_virginica)
+       return  json.dumps(prediction_virginica)
     else:
-       prediction_versicolor = '[íris versicolor]'
-       print(prediction_versicolor)
-       return str(prediction_versicolor)
+       prediction_versicolor = 'íris versicolor'
+       #print(prediction_versicolor)
+       return json.dumps(prediction_versicolor)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
